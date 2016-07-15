0672e9d4fc43a06d4a018b5bb116b87e3faf73e9
Vajahath Ahmed <vajuoff.1@gamil.com>
2016-07-15 13:08:45 +0530

    adding google OAuth sign in, sent authcode to our server

---
+++ pf/oofme/custom_modules/config/authCredentials.js
@@ -0,0 +1,11 @@
+// Authentication credentials
+
+module.exports = {
+	
+	// of Google
+	googleAuth: {
+		clientID: '992052124387-ivjgr69teciklhiga8em2p2lgmak3tec.apps.googleusercontent.com',
+		clientSecret: 'qpeuRYVOJCsEWfh1x9bTJFbi',
+		callbackURL: 'http://localhost:3001/auth/google/callback'
+	}
+};
--- pf/oofme/routes/index.js
+++ pf/oofme/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.sendFile('land.html', { root: path.join(__dirname, '../views') });
 });
 
+router.post('/auth/google/storeauthcode', function(req, res){
+	console.log(req.body);
+})
 module.exports = router;
