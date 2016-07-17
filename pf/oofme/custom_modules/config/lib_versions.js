0e7b99bed9685a9eb8a8919af45b59669b62745d
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-07-17 08:01:15 +0530

    Setting up envr for developing ui with angular material

---
+++ pf/oofme/custom_modules/config/lib_versions.js
@@ -0,0 +1,20 @@
+// Application Library Versions being used.
+// Test before deploy.
+
+module.exports = {
+
+	// Angular JS
+	angularJS: "1.5.7",
+
+	// Angular-Animate
+	angular_animate: "1.5.7",
+
+	// Angular-Area
+	angular_area: "1.5.7",
+
+	// Angular-Messages
+	angular_messages: "1.5.7",
+
+	// Angular-Matrial
+	angular_material: "1.1.0"
+};
--- pf/oofme/routes/index.js
+++ pf/oofme/routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
-var router = express.Router();
 var path = require('path');
+var libV = require('../custom_modules/config/lib_versions.js');
+
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.sendFile('land.html', { root: path.join(__dirname, '../views') });
+	res.render('index', {
+		title: "oofme",
+		v_angularjs: libV.angularJS,
+		v_angularMaterial: libV.angular_material,
+		v_angularArea: libV.angular_area,
+		v_angularAnimate: libV.angular_animate,
+		v_angularMessages: libV.angular_messages
+	});
 });
 
 router.get('/profile', function(req, res){
+	console.log(JSON.stringify(req.session, null, 4));
 	res.send("profile.");
-})
+});
+
+router.get('/u', function(req, res){
+	res.sendFile('land.html', { root: path.join(__dirname, '../views') });
+});
+
 module.exports = router;
