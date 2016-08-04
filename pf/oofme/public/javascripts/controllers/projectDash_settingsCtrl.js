fc56c793bef402336d6ea79ba9b22ebfc50310a0
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-08-04 23:06:28 +0530

    some corrections

--- pf/oofme/public/javascripts/controllers/projectDash_settingsCtrl.js
+++ pf/oofme/public/javascripts/controllers/projectDash_settingsCtrl.js
@@ -6,15 +6,15 @@
 angular.module('oofme')
 
 .controller('projectDash_settingsCtrl', ['$scope', '$rootScope', '$state', '$mdDialog', '$mdMedia', 'Store', function($scope, $rootScope, $state, $mdDialog, $mdMedia, Store) {
-	// $scope.project = Store.currentLoadedProject;
-	$scope.project = {
-		name: "test",
-		id: 45,
-		tagline: ''
-	};
+	$scope.project = Store.currentLoadedProject;
+	// $scope.project = {
+	// 	name: "test",
+	// 	id: 45,
+	// 	tagline: ''
+	// };
 
-	$scope.applyChanges = function() {
-		Store.currentLoadedProject = $scope.project;
-		console.log(Store.currentLoadedProject);
-	}
+	// $scope.applyChanges = function() {
+	// 	Store.currentLoadedProject = $scope.project;
+	// 	console.log(Store.currentLoadedProject);
+	// }
 }]);
