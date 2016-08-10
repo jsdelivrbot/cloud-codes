4b0c9f392eab4849e8e6d9133f55de89459a4c7c
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-08-10 20:24:03 +0530

    random access to /dash route is blocked and redirected to /

--- pf/oofme/public/javascripts/controllers/projectDashCtrl.js
+++ pf/oofme/public/javascripts/controllers/projectDashCtrl.js
@@ -6,20 +6,25 @@
 angular.module('oofme')
 
 .controller('projectDashCtrl', ['$scope', '$rootScope', '$state', '$mdDialog', '$mdMedia', 'Store', function($scope, $rootScope, $state, $mdDialog, $mdMedia, Store) {
+	if (Store.currentLoadedProject) {
+		// change in Store shold reflect here. So $watch-ing.
+		$scope.$watch(function() {
+			return Store.currentLoadedProject;
+		}, function(newVal, oldVal) {
+			if (!(newVal === undefined)) {
+				$scope.project = Store.currentLoadedProject;
+			}
+		});
+		$scope.currentNavItem = "overview";
+		$state.go('projectDash_overview');
 
-	// change in Store shold reflect here. So $watch-ing.
-	$scope.$watch(function() {
-		return Store.currentLoadedProject;
-	}, function(newVal, oldVal) {
-		if (!(newVal === undefined)) {
-			$scope.project = Store.currentLoadedProject;
-		}
-	});
-	$scope.currentNavItem = "overview";
-	$state.go('projectDash_overview');
-
-	// for handling nav bar
-	$rootScope.$on('$routeChangeSuccess', function(event, current) {
-		$scope.currentLink = getCurrentLinkFromRoute(current);
-	});
+		// for handling nav bar
+		$rootScope.$on('$routeChangeSuccess', function(event, current) {
+			$scope.currentLink = getCurrentLinkFromRoute(current);
+		});
+		
+	} else {
+		$state.go('allProjects');
+		console.log("Undefined Store.currentLoadedProject. So redirecting to 'allProjects' state");
+	}
 }]);
