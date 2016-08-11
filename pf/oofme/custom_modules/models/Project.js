9584668f64fe3ad17e0a9969bafc216a99dfbec0
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-08-11 20:07:13 +0530

    added 2 models for storing data in a structured manner

---
+++ pf/oofme/custom_modules/models/Project.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+// define the schema for our project model
+var projectSchema = mongoose.Schema({
+
+	id: String,
+	name: String,
+	authors: Array,
+	isPublished: Boolean,
+	tagline: String,
+	files: Array
+
+});
+
+
+// create the model for projects and expose it to our app
+module.exports = mongoose.model('Project', projectSchema);
