4ae8fb02d716500316b0264ca9afc6550af3edfe
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-08-28 19:44:37 +0530

    reporters updated

--- pf/oofme/tests/angular/services/StoreFactory.spec.js
+++ pf/oofme/tests/angular/services/StoreFactory.spec.js
@@ -1,6 +1,14 @@
 // tests for StoreFactory.js
-describe('StoreFactory', function(){
-	it('dummy spec', function(){
-		expect(1+2).toEqual(3);
+describe('Store (Factory)', function() {
+	var Store;
+
+	beforeEach(angular.mock.module('oofme'));
+
+	beforeEach(inject(function(_Store_) {
+		Store = _Store_;
+	}));
+
+	it('is defined', function() {
+		expect(Store).toBeDefined();
 	});
 });
