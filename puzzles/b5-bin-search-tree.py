ac2aa6c23bc54190207bd086c6a94e8bc438394d
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-12-18 10:38:17 +0530

    prog qns complete

--- puzzles/b5-bin-search-tree.py
+++ puzzles/b5-bin-search-tree.py
@@ -1,59 +1,26 @@
 # Binary search tree validation
 # =============================
 
-import itertools
-
-# lets create a bin tree
-class Node:
-	lChild = None
-	rChild = None
-	data = None
-
-	def __init__(self, key):
-		self.rChild = None
-		self.lChild = None
-		self.data = key
-	def __str__(self):
-		return "Node(%d)" %(self.data)
-
-class Tree:
-	root = None
-
-	def __str__(self):
-		return "Tree"
-
-	def __init__(self):
-		self.root = None
-
-# manually creating binary tree instead of
-# writing a separate function for insertion
-# for the purpose of demonstration and testing.(see docs)
-t = Tree()
-t.root = Node(5)
-t.root.lChild = Node(3)
-t.root.lChild.lChild = Node(1)
-t.root.lChild.rChild = Node(4)
-t.root.rChild = Node(7)
-t.root.rChild.lChild = Node(7)
-t.root.rChild.rChild = Node(10)
-
-depth = 3
-
-print(t.root)
-print(t.root.lChild)
-print(t.root.lChild.lChild)
-print(t.root.lChild.rChild)
-print(t.root.rChild)
-print(t.root.rChild.lChild)
-print(t.root.rChild.rChild)
+# Function to find depth
+def depth(t):
+	if t == None:
+		return 0
+	else:
+		dlc = depth(t.lChild)
+		drc = depth(t.rChild)
+		if dlc>=drc:
+			mxd = dlc + 1
+		else:
+			mxd = drc + 1
+	return mxd
 
 # Function to determine if the BST is valid
 def validateBTree(t):
 	# Find depth
-	
+	d = depth(t)
 	# Construct a flattend array
 	values = [t.lChild, t.data, t.rChild]
-	for i in range(1, 4):
+	for i in range(0, d):
 		j = 0
 		while j<len(values):
 			if hasattr(values[j], 'lChild') or hasattr(values[j], 'rChild'):
@@ -81,5 +48,83 @@ def validateBTree(t):
 	
 	return True
 
-# Tests
-print(validateBTree(t.root)) # should return true
+###################################################/
+
+# lets create a bin tree
+class Node:
+	lChild = None
+	rChild = None
+	data = None
+
+	def __init__(self, key):
+		self.rChild = None
+		self.lChild = None
+		self.data = key
+	def __str__(self):
+		return "Node(%d)" %(self.data)
+
+class Tree:
+	root = None
+
+	def __str__(self):
+		return "Tree"
+
+	def __init__(self):
+		self.root = None
+
+# manually creating binary tree instead of
+# writing a separate function for insertion
+# for the purpose of demonstration and testing.(see docs)
+# ========================================================
+# ** valid BST
+#                        ROOT
+#                         |
+#                         V
+# 						  5
+# 					  /      \
+# 				    3         7
+# 				 /   \     /    \
+# 			   1      4   6     10
+# 			                      \
+# 			                       11
+
+validBST = Tree()
+validBST.root = Node(5)
+validBST.root.lChild = Node(3)
+validBST.root.lChild.lChild = Node(1)
+validBST.root.lChild.rChild = Node(4)
+validBST.root.rChild = Node(7)
+validBST.root.rChild.lChild = Node(7)
+validBST.root.rChild.rChild = Node(10)
+validBST.root.rChild.rChild.rChild = Node(11)
+
+# ----------------------------------------
+# ** invalid BST
+#                        ROOT
+#                         |
+#                         V
+# 						  5
+# 					  /      \
+# 				   *10        7
+# 				 /   \     /    \
+# 			   1      4  *1     10
+# 			                      \
+# 			                       11
+#
+
+invalidBST = Tree()
+invalidBST.root = Node(5)
+invalidBST.root.lChild = Node(10)
+invalidBST.root.lChild.lChild = Node(1)
+invalidBST.root.lChild.rChild = Node(4)
+invalidBST.root.rChild = Node(7)
+invalidBST.root.rChild.lChild = Node(1)
+invalidBST.root.rChild.rChild = Node(10)
+invalidBST.root.rChild.rChild.rChild = Node(11)
+
+
+# Validating
+print("\n\nchecking validity of 'validBST':")
+print("validateBTree(validBST) = ", validateBTree(validBST.root)) # should return true
+print("\n\nchecking validity of 'invalidBST':")
+print("validateBTree(invalidBST) = ", validateBTree(invalidBST.root)) # should return false
