597c48822aa9544182a2aa82a41acc12af2c9047
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-12-18 09:30:41 +0530

    90% qns completed

---
+++ puzzles/b4-linked-lists.py
@@ -0,0 +1,45 @@
+# Finding cycles in linked lists
+# ==============================
+
+# Function to detect loops
+def isLooped(l):
+	pointers = [0]
+	loop = False
+	for i in l:
+		pointers.append(i[1])
+	pointers.sort()
+	# print(pointers)
+	# loop and detect duplicates
+	for i in range(0, len(pointers)-1):
+		if pointers[i] == pointers[i+1]:
+			loop = True
+			break
+
+	return loop
+
+# generate a linked list with cyle
+ll1 = []
+for i in range(0, 10):
+	if i == 8:
+		ll1.append(("hi", 6))
+	else:
+		ll1.append(("hi", i+1))
+
+# generate a linked list with-out cycle
+ll2 = []
+for i in range(0, 10):
+	ll2.append(("hi", i+1))
+
+
+print("\nTesting linked list with cycle for cycles:")
+if isLooped(ll1)==True:
+	print("Loop detected: SUCCESS")
+else:
+	print("Loop not detected: ERROR")
+
+
+print("\nTesting linked list with without cycle for cycles:")
+if isLooped(ll2)==False:
+	print("Loop not detected: SUCCESS")
+else:
+	print("Loop detected: ERROR")
